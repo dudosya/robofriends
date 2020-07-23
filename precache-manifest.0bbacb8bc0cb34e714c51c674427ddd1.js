0a0394d2b7019be147e76fd3f5fd35a77a4dc67f
unknown <you@example.com>
2020-07-23 15:03:37 +0600

    Updates

---
+++ precache-manifest.0bbacb8bc0cb34e714c51c674427ddd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d6b7e85ac3b4eb72c28291f231e1a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c1dddf4b06ba819b6180",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "62b07398ddf0b7497106",
+    "url": "/robofriends/static/css/main.86032667.chunk.css"
+  },
+  {
+    "revision": "c1dddf4b06ba819b6180",
+    "url": "/robofriends/static/js/2.76453558.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.76453558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b07398ddf0b7497106",
+    "url": "/robofriends/static/js/main.4ecaae2b.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
